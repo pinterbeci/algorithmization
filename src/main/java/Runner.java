df0990837098fab70464a1928b1d6c40eda134f7
pinterbela <pinter.bela@udinfopark.hu>
2025-01-20 15:12:19 +0100

    read that proposal

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import main.java.hu.pinterbeci.algorithms.StringPermutation;
+
+public class Runner {
+    public static void main(String[] args) {
+        // System.out.println(Fibonacci.fibonacci(6));
+        System.out.println(StringPermutation.findAllPermutations("tre"));
+    }
+}
--- src/main/java/hu/pinterbeci/algorithms/Runner.java
+++
@@ -1,7 +0,0 @@
-package main.java.hu.pinterbeci.algorithms;
-
-public class Runner {
-    public static void main(String[] args) {
-        System.out.println(Fibonacci.fibonacci(6));
-    }
-}
