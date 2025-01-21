78724fd1006f94a810278eba5f54aa92a00e57ed
pinterbela <pinter.bela@udinfopark.hu>
2025-01-21 13:05:25 +0100

    Revert "read that proposal"
    This reverts commit df099083

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,7 @@
 package main.java;
 
-
-import main.java.hu.pinterbeci.algorithms.StringPermutation;
-
 public class Runner {
     public static void main(String[] args) {
         // System.out.println(Fibonacci.fibonacci(6));
-        System.out.println(StringPermutation.findAllPermutations("aasc"));
     }
 }
---
+++ src/main/java/hu/pinterbeci/algorithms/Runner.java
@@ -0,0 +1,7 @@
+package main.java.hu.pinterbeci.algorithms;
+
+public class Runner {
+    public static void main(String[] args) {
+        System.out.println(Fibonacci.fibonacci(6));
+    }
+}
