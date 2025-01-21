cc5c10e56c558db793760f022ea6b125f23e996c
pinterbela <pinter.bela@udinfopark.hu>
2025-01-21 13:08:51 +0100

    fixing after commit revert

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,7 +1,11 @@
 package main.java;
 
+import main.java.hu.pinterbeci.algorithms.StringPermutation;
+
 public class Runner {
     public static void main(String[] args) {
         // System.out.println(Fibonacci.fibonacci(6));
+        //System.out.println(StringPermutation.findAllPermutations("tadgasdasjkdncvc_o8541eqgasasd-@|ß$äđĐä"));
+        System.out.println(StringPermutation.findAllPermutations("retetarew"));
     }
 }
